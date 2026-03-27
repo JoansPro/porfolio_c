82971196c40f373bf6dd28896f88ccddeefbc40c
JoansPro <ariellouenzing@gmail.com>
2026-03-27 19:09:01 +0100

    Update projectController.js

--- Backend/controllers/projectController.js
+++ Backend/controllers/projectController.js
@@ -43,10 +43,20 @@ const getProjectById = async (req, res) => {
     }
 };
 
+const updateProject = async (req, res) => {
+    try {
+        const project = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!project) return res.status(404).json({ message: "Project not found" });
+        res.status(200).json(project);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 // Export functions
 module.exports = { 
     getAllProjects, 
     createProject,
     getProjectsByCategory,
-    getProjectById // 🔥 Ajout ici
+    getProjectById,
+    updateProject // 🔥 Ajout ici
 };
