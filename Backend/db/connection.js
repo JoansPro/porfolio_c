c70a5b62842e47325bdc1f2ab5469db6d10f8352
JoansPro <ariellouenzing@gmail.com>
2026-03-28 08:46:32 +0100

    Update connection.js

--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+    if (!process.env.MONGO_URI) {
+    console.error("❌ MONGO_URI manquant !");
+    process.exit(1);
+    }
     console.log("URI:", process.env.MONGO_URI);
     try {
         await mongoose.connect(process.env.MONGO_URI, {
@@ -12,10 +16,7 @@ const connectDB = async () => {
         console.error('MongoDB connection error:', error);
         process.exit(1);
     }
-    if (!process.env.MONGO_URI) {
-    console.error("❌ MONGO_URI manquant !");
-    process.exit(1);
-    }
+    
 };
 
 module.exports = connectDB;
